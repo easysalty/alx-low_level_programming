5bf0b97684c8912453cf728dad1641990a3ed124
easysalty <stephenezra1@gmail.com>
2022-10-14 15:29:02 -0700

    tasks

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,7 +3,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers, followed by a new line.
+ * print_strings- prints strings, followed by a new line.
  * @seperator: seperator to print between strings
  * @n: number of strings to print
  *
